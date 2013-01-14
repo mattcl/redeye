663f1dd88d947073774ddd3faed73e8a8d943efa
Matt Chun-Lum <mchunlum@gmail.com>
2013-01-13 16:34:30 -0800

    working out how the class structure should be

--- lib/redeye/client.rb
+++ lib/redeye/client.rb
@@ -5,6 +5,7 @@ require 'open-uri'
 module Redeye
   class Client
     attr_accessor :base_address, :base_uri
+    attr_reader :serial_number, :hardware_type, :name, :description
 
     def address(addr)
       @base_address = addr
@@ -13,6 +14,22 @@ module Redeye
 
     def info
       xml = Nokogiri::XML(open(base_uri))
+      attrs = xml.children.first.attributes
     end
+
+    def devices
+      # TODO: eventually support redeye pro devices
+      xml = Nokogiri::XML(open("#{base_uri}/rooms/0/devices/"))
+    end
+
+  end
+
+  class Room
+  end
+
+  class Device
+
   end
 end
+
+# redeye/rooms/0/devices/1/commands/send?commandId=10
